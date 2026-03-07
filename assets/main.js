eed552c723234719fd9af53968e00f3ab8529efc
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-03-07 14:19:45 +0000

    Complete modal and navbar styling. Add Javascript skeleton.

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,22 @@
-function initMaps(){}
+document.addEventListener("DOMContentLoaded", () => {
+  initMaps();
+  transparentNavbar();
+  thumbCTAToggle();
+  formValidation();
+  contactFormSuccess();
+  newspaperSubscriptionAlert();
+  filterResults();
+});
+function initMaps() {}
 
+function transparentNavbar() {}
+
+function thumbCTAToggle() {}
+
+function formValidation() {}
+
+function contactFormSuccess() {}
+
+function newspaperSubscriptionAlert() {}
+
+function filterResults() {}
